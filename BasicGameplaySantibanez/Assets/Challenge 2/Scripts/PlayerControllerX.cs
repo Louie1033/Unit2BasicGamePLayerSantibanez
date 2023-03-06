51a8ffd6f2fa32bab3d7064c6399a7220a09c728
Louie1033 <louissantibanez22@gmail.com>
2023-03-06 09:56:28 -0800

    completed unit 2 challenge 2
    began bonus features

---
+++ BasicGameplaySantibanez/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldown = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (cooldown <= 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1.5f;
+        }
+        if(cooldown > 0 )
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+}
--- BasicGameplaySantibanez/Assets/Scripts/PlayerController.cs
+++ BasicGameplaySantibanez/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
-    public float xRange = 15.0f;
+    public float xRange = 20.0f;
+    public float maxZRange = 16.0f;
+    public float minZRange = -1f;
 
     public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,12 +31,29 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.z < minZRange)
+        {
+            transform.position = new Vector3(transform.position.y, transform.position.y, minZRange);
+        }
+
+        if (transform.position.x > maxZRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, maxZRange);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+
     }
 }
